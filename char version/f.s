92ee872162ee3a4082c2ca150d364509c6831272
bWENTURA <maciej.b.iwanski@gmail.com>
2018-06-05 09:03:21 +0200

    1.0 + char

---
+++ char version/f.s
@@ -0,0 +1,490 @@
+section .text
+global f
+
+section .data
+	temp_integer:	times 20 db	0
+	temp_double: times 32 db 0
+	colours:	times 12 db 0
+	temp_coordinates: times 144 db 0
+	temp_calculations: times 72 db 0
+	temp_sorted_pair: times	16 db 0
+	iterator_1: times 4 db 0
+	iterator_2: times 4 db 0
+
+f:
+	push rbp
+	mov rbp, rsp
+
+			;first section - clearing bitmap and z-buffer array
+
+	mov r10, rdx
+	movsxd rax, DWORD[rdx]
+	mov r11, 3
+	mul r11
+	mov rdx, r10
+
+	mov r11d, DWORD[rdx + 8]
+	movsxd r14, r11d
+	mov r11, r14
+
+	movsxd r10, DWORD[rdx + 4]
+	mov r12, rdi
+bitmap_clear_outside_loop:
+	mov r13, rax
+bitmap_clear_inside_loop:
+	mov BYTE[r12], 0xff
+	inc r12
+	dec r13
+	cmp r13, 0
+	jne bitmap_clear_inside_loop
+	mov r12, rdi
+	add r12, r14
+	add r14, r11
+	dec r10
+	cmp r10, 0
+	jne bitmap_clear_outside_loop
+
+
+
+	push rdx
+	movsxd rax, DWORD[rdx]
+	movsxd r10, DWORD[rdx + 4]
+	mul r10
+	pop rdx
+
+	mov r10, rsi
+z_buffer_clear_loop:
+	mov BYTE[r10], 0
+	inc r10
+	dec rax
+	jne z_buffer_clear_loop
+
+			;end of bitmap and -buffer section
+
+			;section sorting array of coordinates in correct order, with lowest y coordinate first
+
+	mov r10d, DWORD[rcx + 4]
+	mov r12d, DWORD[rcx + 16]
+	cmp r10d, r12d
+	jb skip_first
+
+	mov r10d, DWORD[rcx + 16]
+	mov r12d, DWORD[rcx + 4]
+
+skip_first:
+
+	mov r11d, DWORD[rcx + 28]
+	mov r13d, DWORD[rcx + 40]
+	cmp r11d, r13d
+	jb skip_second
+
+	mov r11d, DWORD[rcx + 40]
+	mov r13d, DWORD[rcx + 28]
+
+skip_second:
+
+	cmp r10d, r11d
+	jb skip_third
+
+	mov r15d, r10d
+	mov r10d, r11d
+	mov r11d, r15d
+
+skip_third:
+
+	cmp r12d, r13d
+	jb skip_fourth
+
+	mov r15d, r12d
+	mov r12d, r13d
+	mov r13d, r15d
+
+skip_fourth:
+
+	cmp r11d, r12d
+	jb skip_fifth
+
+	mov r15d, r11d
+	mov r11d, r12d
+	mov r12d, r15d
+
+skip_fifth:
+
+	mov r15, temp_integer
+	mov DWORD[r15], r10d
+	mov DWORD[r15 + 4], r11d
+	mov DWORD[r15 + 8], r12d
+	mov DWORD[r15 + 12], r13d
+
+
+	mov r10, 3
+	mov r11, rcx
+	add r11, 4
+	mov r12, temp_integer
+swift_loop_first:
+	mov r13d, DWORD[r12]
+	mov r14, r11
+swift_loop_second:
+	add r14, 12
+	; movsxd rbx, DWORD[r14]
+	; mov QWORD[r9], rb
+	cmp r13d, DWORD[r14 - 12]
+	jne swift_loop_second
+
+	sub r14, 12
+	mov r15d, DWORD[r11]
+	mov r13d, DWORD[r14]
+	mov DWORD[r14], r15d
+	mov DWORD[r11], r13d
+	mov r15d, DWORD[r11 + 4]
+	mov r13d, DWORD[r14 + 4]
+	mov DWORD[r14 + 4], r15d
+	mov DWORD[r11 + 4], r13d
+	mov r15d, DWORD[r11 - 4]
+	mov r13d, DWORD[r14 - 4]
+	mov DWORD[r14 - 4], r15d
+	mov DWORD[r11 - 4], r13d
+
+	add r11, 12
+	add r12, 4
+	dec r10
+	cmp r10, 0
+	jne swift_loop_first
+
+			;end of section which sort coordinates
+
+			;store sets of coordinates
+
+  mov r10, rcx
+	mov rax, temp_coordinates
+	mov r12, 2
+	mov r13, 1
+	sub r10, 12
+loop_sets_outside:
+	add r13, 1
+	add r10, 12
+	mov r15, r10
+loop_sets_inside:
+	add r15, 12
+	mov r11d, DWORD[r10]
+	mov DWORD[rax], r11d
+	mov r11d, DWORD[r10 + 4]
+	mov DWORD[rax + 4], r11d
+	mov r11d, DWORD[r10 + 8]
+	mov DWORD[rax + 8], r11d
+	mov r11d, DWORD[r15]
+	mov DWORD[rax + 12], r11d
+	mov r11d, DWORD[r15 + 4]
+	mov DWORD[rax + 16], r11d
+	mov r11d, DWORD[r15 + 8]
+	mov DWORD[rax + 20], r11d
+	mov r11d, DWORD[r15 + 12]
+	mov DWORD[rax + 24], r11d
+	mov r11d, DWORD[r15 + 16]
+	mov DWORD[rax + 28], r11d
+	mov r11d, DWORD[r15 + 20]
+	mov DWORD[rax + 32], r11d
+	add rax, 36
+	dec r13
+	cmp r13, 0
+	jne loop_sets_inside
+	dec r12
+	cmp r12, 0
+	jne loop_sets_outside
+
+	mov r10, rcx
+	mov r11d, DWORD[r10]
+	mov DWORD[rax], r11d
+	mov r11d, DWORD[r10 + 4]
+	mov DWORD[rax + 4], r11d
+	mov r11d, DWORD[r10 + 8]
+	mov DWORD[rax + 8], r11d
+	mov r11d, DWORD[r10 + 12]
+	mov DWORD[rax + 12], r11d
+	mov r11d, DWORD[r10 + 16]
+	mov DWORD[rax + 16], r11d
+	mov r11d, DWORD[r10 + 20]
+	mov DWORD[rax + 20], r11d
+	mov r11d, DWORD[r10 + 36]
+	mov DWORD[rax + 24], r11d
+	mov r11d, DWORD[r10 + 40]
+	mov DWORD[rax + 28], r11d
+	mov r11d, DWORD[r10 + 44]
+	mov DWORD[rax + 32], r11d
+
+			;temp_coordinates set is good
+
+			;section filling the bitmap with values
+
+	push r9
+
+	mov BYTE[colours], 255
+	mov BYTE[colours + 1], 0
+	mov BYTE[colours + 2], 0
+	mov BYTE[colours + 3], 0
+	mov BYTE[colours + 4], 0
+	mov BYTE[colours + 5], 255
+	mov BYTE[colours + 6], 0
+	mov BYTE[colours + 7], 255
+	mov BYTE[colours + 8], 0
+	mov BYTE[colours + 9], 127
+	mov BYTE[colours + 10], 127
+	mov BYTE[colours + 11], 127
+
+	mov r12d, DWORD[rdx]
+	mov DWORD[temp_integer], r12d
+	mov r12d, DWORD[rdx + 8]
+	mov DWORD[temp_integer + 4], r12d
+
+	mov DWORD[iterator_2], 2
+	mov rbx, temp_coordinates
+	sub rbx, 36
+	mov r15, colours
+	sub r15, 3
+
+loop_outside:
+	add rbx, 36
+	add r15, 3
+
+		;calculating a and b from y = a*x + b
+
+	movsxd r10, DWORD[rbx]
+	movsxd r11, DWORD[rbx + 12]
+	movsxd r12, DWORD[rbx + 4]
+	movsxd r13, DWORD[rbx + 16]
+	sub r10, r11
+	sub r12, r13
+
+	cvtsi2sd xmm1, r10
+	cvtsi2sd xmm0, r12
+	divsd xmm0, xmm1
+	cvtsi2sd xmm2, DWORD[rbx]
+	cvtsi2sd xmm1, DWORD[rbx + 4]
+	mulsd xmm2, xmm0
+	subsd xmm1, xmm2
+	movsd QWORD[temp_calculations], xmm0
+	movsd QWORD[temp_calculations + 8], xmm1
+
+	movsxd r10, DWORD[rbx]
+	movsxd r11, DWORD[rbx + 24]
+	movsxd r12, DWORD[rbx + 4]
+	movsxd r13, DWORD[rbx + 28]
+	sub r10, r11
+	sub r12, r13
+
+	cvtsi2sd xmm1, r10
+	cvtsi2sd xmm0, r12
+	divsd xmm0, xmm1
+	cvtsi2sd xmm2, DWORD[rbx]
+	cvtsi2sd xmm1, DWORD[rbx + 4]
+	mulsd xmm2, xmm0
+	subsd xmm1, xmm2
+	movsd QWORD[temp_calculations + 16], xmm0
+	movsd QWORD[temp_calculations + 24], xmm1
+
+	movsxd r10, DWORD[rbx + 12]
+	movsxd r11, DWORD[rbx + 24]
+	movsxd r12, DWORD[rbx + 16]
+	movsxd r13, DWORD[rbx + 28]
+	sub r10, r11
+	sub r12, r13
+
+	cvtsi2sd xmm1, r10
+	cvtsi2sd xmm0, r12
+	divsd xmm0, xmm1
+	cvtsi2sd xmm2, DWORD[rbx + 12]
+	cvtsi2sd xmm1, DWORD[rbx + 16]
+	mulsd xmm2, xmm0
+	subsd xmm1, xmm2
+	movsd QWORD[temp_calculations + 32], xmm0
+	movsd QWORD[temp_calculations + 40], xmm1
+
+		;end of calculating a and b from y = a*x + b
+
+		;calculating thing for z-buffer
+
+	cvtsi2sd xmm0, DWORD[rbx + 8]				;z0
+	cvtsi2sd xmm1, DWORD[rbx + 20]			;z1
+	cvtsi2sd xmm2, DWORD[rbx + 32]			;z2
+	cvtsi2sd xmm3, DWORD[rbx + 4]				;y0
+	cvtsi2sd xmm4, DWORD[rbx + 16]			;y1
+	cvtsi2sd xmm5, DWORD[rbx + 28]			;y2
+	subsd xmm1, xmm0
+	subsd xmm2, xmm0
+	subsd xmm4, xmm3
+	subsd xmm5, xmm3
+	mulsd xmm1, xmm5
+	mulsd xmm2, xmm4
+	subsd xmm1, xmm2
+	movsd QWORD[temp_calculations + 48], xmm1
+
+	cvtsi2sd xmm0, DWORD[rbx + 8]				;z0
+	cvtsi2sd xmm1, DWORD[rbx + 20]			;z1
+	cvtsi2sd xmm2, DWORD[rbx + 32]			;z2
+	cvtsi2sd xmm3, DWORD[rbx]						;x0
+	cvtsi2sd xmm4, DWORD[rbx + 12]			;x1
+	cvtsi2sd xmm5, DWORD[rbx + 24]			;x2
+	subsd xmm1, xmm0
+	subsd xmm2, xmm0
+	subsd xmm4, xmm3
+	subsd xmm5, xmm3
+	mulsd xmm1, xmm5
+	mulsd xmm2, xmm4
+	subsd xmm2, xmm1
+	movsd QWORD[temp_calculations + 56], xmm2
+
+	cvtsi2sd xmm0, DWORD[rbx]						;x0
+	cvtsi2sd xmm1, DWORD[rbx + 12]			;x1
+	cvtsi2sd xmm2, DWORD[rbx + 24]			;x2
+	cvtsi2sd xmm3, DWORD[rbx + 4]				;y0
+	cvtsi2sd xmm4, DWORD[rbx + 16]			;y1
+	cvtsi2sd xmm5, DWORD[rbx + 28]			;y2
+	subsd xmm1, xmm0
+	subsd xmm2, xmm0
+	subsd xmm4, xmm3
+	subsd xmm5, xmm3
+	mulsd xmm1, xmm5
+	mulsd xmm2, xmm4
+	subsd xmm1, xmm2
+	movsd QWORD[temp_calculations + 64], xmm1
+
+		;end of calculating things for z-buffer
+
+	mov r9d, DWORD[rbx + 4]
+	mov DWORD[temp_integer + 8], r9d
+	mov r9d, DWORD[rbx + 28]
+	mov DWORD[temp_integer + 12], r9d
+	mov r9d, DWORD[rbx + 16]
+
+	mov DWORD[iterator_1], 2
+	mov rax, temp_calculations
+	sub rax, 16
+
+loop_middle:
+	add rax, 16
+	dec DWORD[temp_integer + 8]
+loop_inside:
+	mov r10, rdi
+	inc DWORD[temp_integer + 8]
+
+	cvtsi2sd xmm2, DWORD[temp_integer + 4]
+	cvtsi2sd xmm3, DWORD[temp_integer + 8]
+	mulsd xmm2, xmm3
+
+	cvtsi2sd xmm0, DWORD[temp_integer + 8]
+	subsd xmm0, [rax + 8]
+	divsd xmm0, [rax]
+	cvtsd2si r11, xmm0
+
+	cvtsi2sd xmm1, DWORD[temp_integer + 8]
+	subsd xmm1, [rax + 24]
+	divsd xmm1, [rax + 16]
+	cvtsd2si r12, xmm1
+
+	cmp r11, r12
+	jb skip_change
+
+	mov r13, r11
+	mov r11, r12
+	mov r12, r13
+
+skip_change:
+	mov r14, r11
+	sub r12, r11
+	inc r12	;;;what depends?
+
+	cvtsi2sd xmm0, r11
+	mov r11, 3
+	cvtsi2sd xmm1, r11
+	mulsd xmm1, xmm0
+	addsd xmm1, xmm2
+	cvtsd2si r11, xmm1
+
+	add r10, r11
+
+	cvtsi2sd xmm0, DWORD[temp_integer + 8]
+	cvtsi2sd xmm1, DWORD[rdx]
+	mulsd xmm0, xmm1
+	cvtsd2si r11, xmm0
+
+	add r11, r14
+	mov QWORD[temp_double], r11
+	mov r11, rsi
+	add r11, QWORD[temp_double]
+
+	cvtsi2sd xmm0, DWORD[temp_integer + 8]		;y
+	cvtsi2sd xmm1, DWORD[rbx + 4]							;y0
+	movsd xmm2, QWORD[temp_calculations + 56]
+
+	subsd xmm0, xmm1
+	mulsd xmm0, xmm2
+
+	cvtsi2sd xmm3, DWORD[rbx + 8]							;z0
+	cvtsi2sd xmm2, DWORD[rbx]									;x0
+
+colour_loop:
+	push r14
+	cvtsi2sd xmm1, r14												;x
+	movsd xmm4, QWORD[temp_calculations + 48]
+	movsd xmm5, QWORD[temp_calculations + 64]
+
+	subsd xmm1, xmm2
+	mulsd xmm1, xmm4
+	addsd xmm1, xmm0
+	divsd xmm1, xmm5
+	addsd xmm1, xmm3
+	cvtsd2si r14, xmm1
+
+	cmp r14b, BYTE[r11]
+	jb skip_colour
+	;HERE check z-buffer and colour if possible
+
+	mov BYTE[r11], r14b
+	mov r14b, BYTE[r15]
+	mov BYTE[r10], r14b
+	mov r14b, BYTE[r15 + 1]
+	mov BYTE[r10 + 1], r14b
+	mov r14b, BYTE[r15 + 2]
+	mov BYTE[r10 + 2], r14b
+
+
+skip_colour:
+	add r10, 3
+	pop r14
+	inc r14
+	inc r11
+	dec r12
+	cmp r12, 0
+	jne colour_loop
+
+	cmp DWORD[temp_integer + 8], r9d
+	jne loop_inside
+
+	mov r9d, DWORD[temp_integer + 12]
+
+	sub DWORD[iterator_1], 1
+	jne loop_middle
+
+	dec DWORD[iterator_2]
+	jne loop_outside
+;
+	pop r9
+; 	mov rbx, temp_coordinates
+	; movsd xmm1, QWORD[temp_calculations + 56]
+	movq [r9], xmm1
+
+
+
+
+
+
+
+
+
+
+			;end of second section
+exit:
+
+	mov rsp, rbp
+	pop rbp
+	ret
---
+++ char version/main.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro5/allegro.h>
+#include </home/mylittlemachine/allegro5/include/allegro5/internal/aintern_bitmap.h>
+// #include "allegro5/internal/aintern_bitmap.h"
+#include "f.h"
+
+const int SCREEN_W = 512;
+const int SCREEN_H = 512;
+const int XYMATRIX_WIDTH = 512;
+const int XYMATRIX_HEIGHT = 512;
+enum ANGLES{
+  SWIFT_UP, SWIFT_DOWN, SWIFT_LEFT, SWIFT_RIGHT
+};
+
+int main()
+{
+  ALLEGRO_DISPLAY *display = NULL;
+  ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+  ALLEGRO_BITMAP *xy_matrix = NULL;
+  bool redraw = true;
+  int coordinates[12] = {60, 100, 55,
+                        200, 200, 10,
+                        100, 250, 100,
+                         70, 30, 10};
+  int bitmap_info[3] = {XYMATRIX_WIDTH, XYMATRIX_HEIGHT};
+  char * z_buffer;
+  z_buffer = malloc(sizeof(*z_buffer) * XYMATRIX_WIDTH * XYMATRIX_WIDTH);
+  int angles[4] = {0, 0, 0, 0};
+
+  if(!al_init()) {
+    fprintf(stderr, "failed to initialize allegro!\n");
+    return -1;
+  }
+
+  if(!al_install_keyboard()) {
+    fprintf(stderr, "failed to initialize the keyboard!\n");
+    return -1;
+  }
+
+  al_set_new_bitmap_format(ALLEGRO_PIXEL_FORMAT_BGR_888);
+  display = al_create_display(SCREEN_W, SCREEN_H);
+  if(!display) {
+    fprintf(stderr, "failed to create display!\n");
+    return -1;
+  }
+
+  xy_matrix = al_create_bitmap(XYMATRIX_WIDTH, XYMATRIX_HEIGHT);
+  if(!xy_matrix) {
+    fprintf(stderr, "failed to create bouncer bitmap!\n");
+    al_destroy_display(display);
+    return -1;
+  }
+  al_set_target_bitmap(xy_matrix);
+  al_clear_to_color(al_map_rgb(255, 0, 255));
+  al_set_target_bitmap(al_get_backbuffer(display));
+  event_queue = al_create_event_queue();
+  if(!event_queue) {
+    fprintf(stderr, "failed to create event_queue!\n");
+    al_destroy_bitmap(xy_matrix);
+    al_destroy_display(display);
+    return -1;
+  }
+  al_register_event_source(event_queue, al_get_display_event_source(display));
+  al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+  int a = 0;
+
+
+  do{
+    ALLEGRO_EVENT ev;
+    al_wait_for_event(event_queue, &ev);
+    if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE || ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
+      break;
+    }
+    else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+      switch(ev.keyboard.keycode) {
+        case ALLEGRO_KEY_UP:{
+          angles[SWIFT_UP] = 10;
+          break;
+        }
+        case ALLEGRO_KEY_DOWN:{
+          angles[SWIFT_DOWN] = 10;
+          break;
+        }
+        case ALLEGRO_KEY_LEFT:{
+          angles[SWIFT_LEFT] = 10;
+          break;
+        }
+        case ALLEGRO_KEY_RIGHT:{
+          angles[SWIFT_RIGHT] = 10;
+          break;
+        }
+      }
+      redraw = true;
+      al_unregister_event_source(event_queue, al_get_keyboard_event_source());
+    }
+    if(redraw){
+      for(int i = 0; i < 4; ++i) printf("x[%d]=%d, y[%d]=%d, z[%d]=%d\n", i, coordinates[i*3], i, coordinates[i*3 + 1], i, coordinates[i*3 + 2]);
+      ALLEGRO_LOCKED_REGION *locked;
+      unsigned char *data;
+      // unsigned int temp;
+      double temp = 3.0;
+      locked = al_lock_bitmap(xy_matrix, ALLEGRO_PIXEL_FORMAT_BGR_888, ALLEGRO_LOCK_READWRITE);
+      data = locked->data;
+      bitmap_info[2] = locked->pitch;
+      f(data, z_buffer, bitmap_info, coordinates, angles, &temp);
+      // printf("%d\n", locked->pitch);
+      printf("%lf\n", temp);
+      // for(int j = 0; j < 256; ++j){
+      //   for(int i = 0; i < 256; ++i){
+      //     data[i*3 + j*locked->pitch] = 255;
+      //     data[i*3 + j*locked->pitch + 1] = 255;
+      //     data[i*3 + j*locked->pitch + 2] = 255;
+      //   }
+      // }
+
+      data = NULL;
+      al_unlock_bitmap(xy_matrix);
+      for(int i = 0; i < 4; ++i){
+        angles[i] = 0;
+      }
+      redraw = false;
+      al_clear_to_color(al_map_rgb(0,0,0));
+      al_draw_bitmap(xy_matrix, a, a, 0);
+      a += 64;
+      a %= 318;
+      al_flip_display();
+      al_register_event_source(event_queue, al_get_keyboard_event_source());
+    }
+
+  }  while(1);
+  // for(int i = 0; i < 256*256; ++i) printf("%c", z_buffer[i]);
+
+  free(z_buffer);
+  al_destroy_bitmap(xy_matrix);
+  al_destroy_display(display);
+  al_destroy_event_queue(event_queue);
+  return 0;
+}
